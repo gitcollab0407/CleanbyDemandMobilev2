60a14774e10aa5185c925e17646cc60f540f3edb
wilson <mendiolawilson@yahoo.com>
2018-09-22 01:55:53 +0800

    IDK

--- app/src/main/java/com/ignis/cleanbydemandmobile/HistoryFragment.java
+++ app/src/main/java/com/ignis/cleanbydemandmobile/HistoryFragment.java
@@ -123,7 +123,7 @@ public class HistoryFragment extends Fragment {
                                          jsonObject.getString("location") + "_-/" +
                                          jsonObject.getString("cleaner") + "_-/"+
                                          jsonObject.getString("payment_method") + "_-/" +
-                                         jsonObject.getString("rate"));
+                                         jsonObject.getString("name"));
 
 
                 }
